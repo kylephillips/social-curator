c63b9ebe10a97537d3e35a6e35b04a9d9e30a1a3
Kyle Phillips <kyle@object9.com>
2015-06-09 16:29:51 -0400

    Added table for import error logs.

---
+++ app/Activation/Migrations/CreateTables.php
@@ -0,0 +1,63 @@
+<?php namespace SocialCurator\Activation\Migrations;
+
+use SocialCurator\Helpers;
+
+/**
+* Create the Trashed Social Posts Table
+* Saves posts that are trashed so they are not imported on future imports
+*/
+class CreateTables {
+
+	public function __construct()
+	{
+		register_activation_hook( Helpers::plugin_file(), array($this, 'createTables') );
+	}
+
+	/**
+	* Create the DB tables
+	*/
+	public function createTables()
+	{
+		$this->createTrashedTable();
+		$this->createLogsTable();
+	}
+
+	/**
+	* Create the Table to log trashed posts
+	*/
+	private function createTrashedTable()
+	{
+		global $wpdb;
+		$tablename = $wpdb->prefix . 'social_curator_trashed_posts';
+		if ( $wpdb->get_var("SHOW TABLES LIKE '" . $tablename . "'") != $tablename ) :
+			$sql = 'CREATE TABLE ' . $tablename . '(
+				id INTEGER(10) UNSIGNED AUTO_INCREMENT,
+				time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+				site VARCHAR(255) COLLATE utf8mb4_unicode_ci,
+				post_id LONGTEXT COLLATE utf8mb4_unicode_ci,
+				PRIMARY KEY  (id) )';
+			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+			dbDelta($sql);
+		endif;
+	}
+
+	/**
+	* Create the Logs Table
+	*/
+	private function createLogsTable()
+	{
+		global $wpdb;
+		$tablename = $wpdb->prefix . 'social_curator_logs';
+		if ( $wpdb->get_var("SHOW TABLES LIKE '" . $tablename . "'") != $tablename ) :
+			$sql = 'CREATE TABLE ' . $tablename . '(
+				id INTEGER(10) UNSIGNED AUTO_INCREMENT,
+				time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+				type VARCHAR(255) DEFAULT "import-error",
+				message LONGTEXT COLLATE utf8mb4_unicode_ci,
+				PRIMARY KEY  (id) )';
+			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+			dbDelta($sql);
+		endif;
+	}
+
+}
